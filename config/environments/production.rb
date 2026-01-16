2165ae4aa7c74430cb4d5aa562a0fca3ac56c0ee
juankalleo <juankalleo4@gmail.com>
2026-01-16 04:25:21 -0300

    add

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,11 +20,13 @@ Rails.application.configure do
   # Apache or NGINX already handles this.
   config.public_file_server.enabled = ENV["RAILS_SERVE_STATIC_FILES"].present?
 
-  # Compress CSS on deploy.
-  config.assets.css_compressor = nil
+  # Compress CSS on deploy (only if asset pipeline is enabled).
+  if config.respond_to?(:assets)
+    config.assets.css_compressor = nil
 
-  # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+    # Do not fallback to assets pipeline if a precompiled asset is missed.
+    config.assets.compile = false
+  end
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.asset_host = "http://assets.example.com"
