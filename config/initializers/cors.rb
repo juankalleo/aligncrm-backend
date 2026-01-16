0ff62fd1766d866c57ea13f3368012be182c74e2
juankalleo <juankalleo4@gmail.com>
2026-01-16 03:17:47 -0300

    configurar CORS para produção

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -13,12 +13,15 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
       max_age: 86400
   end
 
-  # Produção (configurar domínio real)
-  # allow do
-  #   origins "https://aligncrm.com"
-  #   resource "*",
-  #     headers: :any,
-  #     methods: [:get, :post, :put, :patch, :delete, :options, :head],
-  #     credentials: true
-  # end
+  # Produção
+  allow do
+    origins "https://aligncrm.com.br", "https://www.aligncrm.com.br"
+
+    resource "*",
+      headers: :any,
+      methods: [:get, :post, :put, :patch, :delete, :options, :head],
+      expose: ["Authorization", "X-Total-Count", "X-Total-Pages"],
+      credentials: true,
+      max_age: 86400
+  end
 end
