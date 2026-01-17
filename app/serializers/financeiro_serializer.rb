b60c8083cbd735a4ece7ecbe4e3182a3099b661f
André Dhiogo De Aguiar Muiva <dhiogoandre26@gmail.com>
2026-01-17 19:56:52 -0400

    add

--- app/serializers/financeiro_serializer.rb
+++ app/serializers/financeiro_serializer.rb
@@ -1,5 +1,5 @@
 class FinanceiroSerializer < ActiveModel::Serializer
   attributes :id, :projeto_id, :categoria, :tipo, :descricao, :valor, :data, :vencimento, :pago, :created_at, :updated_at
 
-  belongs_to :projeto, serializer: ProjetoSerializer
+  belongs_to :projeto, serializer: ProjetoSerializer, optional: true
 end
