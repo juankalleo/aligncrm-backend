27903baaec46c1c8dfc0ca45215a9f32e72b9309
juankalleo <juankalleo4@gmail.com>
2026-01-20 04:36:34 -0300

    wip

--- db/migrate/20260117000003_make_projeto_optional_in_financeiros.rb
+++ db/migrate/20260117000003_make_projeto_optional_in_financeiros.rb
@@ -1,4 +1,4 @@
-class MakeProjetoOptionalInFinanceiros < ActiveRecord::Migration[7.1]
+class MakeProjetoOptionalInFinanceirosV2 < ActiveRecord::Migration[7.1]
   def change
     change_column_null :financeiros, :projeto_id, true
   end
